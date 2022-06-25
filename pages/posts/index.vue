07dc74c1acc5619f3c15e352ad3a6837f6b82fbf
ahmadDev <ahmadjafari6050@gmail.com>
2022-06-26 00:21:19 +0430

    3 - 14 final changes

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="posts-page">
-    <PostList />
+    <PostList :posts="loadedPosts" />
   </div>
 </template>
 
 <script>
-import PostList from '@/components/Posts/PostList'
+import PostList from "@/components/Posts/PostList";
 
 export default {
   components: {
     PostList
+  },
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+       resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+            }
+          ]
+        });
+      }, 1000);
+      // reject(new Error())
+    })
+    .then(data => {
+      return data
+    })
+    .catch(e => {
+      context.error(e);
+    });
   }
-}
+};
 </script>
 
 

319fdb7bdc082ee479035216c7cdf2646541eac4
ahmadDev <ahmadjafari6050@gmail.com>
2022-06-25 23:40:36 +0430

    3 - 14

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="single-post-page">
-        <section class="post">
-            <h1 class="post-title">Title of post</h1>
-            <div class="post-details">
-                <div class="post-detail">Last update on XXX</div>
-                <div class="post-detail">Written by NAME</div>
-            </div>
-            <p class="post-content">Content of the post</p>
-        </section>
-        <section class="post-feedback">
-            <p>Let me know your opinion about this post <a href="mailto:dummy@dummy.com">mailto:dummy@dummy.com</a></p>
-        </section>
-
-    </div>
+  <div class="single-post-page">
+    <section class="post">
+      <h1 class="post-title">Title of the Post</h1>
+      <div class="post-details">
+        <div class="post-detail">Last updated on XXX</div>
+        <div class="post-detail">Written by NAME</div>
+      </div>
+      <p class="post-content">Content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
+    </section>
+  </div>
 </template>
+
 <style scoped>
 .single-post-page {
   padding: 30px;
@@ -67,3 +67,4 @@
   color: salmon;
 }
 </style>
+

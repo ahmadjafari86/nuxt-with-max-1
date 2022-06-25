3fca13bd579b3e52fd258f2f7fdd538b5047ad48
ahmadDev <ahmadjafari6050@gmail.com>
2022-06-25 11:34:25 +0430

    init commit

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="about-container">
+        <p>jhajkdhjsahdjkshdjkshjkdhsjdhsdhhjhskdjshdkhdhd</p>
+        <p>jhajkdhjsahdjkshdjkshjkdhsjdhsdhhjhskdjshdkhdhd</p>
+        <p>jhajkdhjsahdjkshdjkshjkdhsjdhsdhhjhskdjshdkhdhd</p>
+    </div> 
+</template>
+<style scoped>
+.about-container {
+    text-align: center;
+    padding: 2rem;
+}
+</style>

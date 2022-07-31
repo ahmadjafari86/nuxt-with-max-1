1701f37e7d3c9724d036d257ac23c03b1929f792
ahmadDev <ahmadjafari6050@gmail.com>
2022-08-01 00:27:51 +0430

    couese part 5

--- components/Admin/AdminPostForm.vue
+++ components/Admin/AdminPostForm.vue
@@ -2,10 +2,15 @@
   <form @submit.prevent="onSave">
     <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
     <AppControlInput v-model="editedPost.title">Title</AppControlInput>
-    <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+    <AppControlInput v-model="editedPost.thumbnail">Thumbnail Link</AppControlInput>
     <AppControlInput
       control-type="textarea"
-      v-model="editedPost.content">Content</AppControlInput>
+      v-model="editedPost.content">Content
+    </AppControlInput>
+    <AppControlInput
+      control-type="textarea"
+      v-model="editedPost.previewText">previewText
+    </AppControlInput>
     <AppButton type="submit">Save</AppButton>
     <AppButton
       type="button"
@@ -37,15 +42,16 @@ export default {
         : {
             author: "",
             title: "",
-            thumbnailLink: "",
-            content: ""
+            thumbnail: "",
+            content: "",
+            previewText: ""
           }
     };
   },
   methods: {
     onSave() {
       // Save the post
-      console.log(this.editedPost);
+      this.$emit('submit', this.editedPost);
     },
     onCancel() {
       // Navigate back
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
-  }
+  },
+  methods: {
+    onSubmitted (postData) {
+      axios.post('https://nuxt-blog-max-70b06-default-rtdb.firebaseio.com/posts.json', {...postData, updatedDate: new Date()})
+        .then(results => console.log(results))
+        .catch(e => console.log(e))
+    }
+  },
 };
 </script>
 
